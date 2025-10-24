51b5e49d54cb13e39b3c994dfa06a8f6ee6b5c46
Hritik086 <151951067+Hritik086@users.noreply.github.com>
2025-10-24 10:26:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -40,7 +40,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 # ── Git / updates ──────────────────────────────────────────────────────────────
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/itskrishname/My-AnnieXMusic.git")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Hritik086/My-AnnieXMusic")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv("GIT_TOKEN")  # needed if repo is private
 
